5d4353caf12e60079d134f1bd5f85e878b487a51
kimberly <maldakimberly228@gmail.com>
2025-03-03 17:45:48 +0300

    creates a file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+const dodger=document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left.replace("px",""));
+    if (left<360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
